cfa80d8ad5c45413063a65d97c081e09f4003352
Erick <netstereo1@gmail.com>
2018-10-12 23:40:37 -0500

    Updates

---
+++ precache-manifest.68b6391664efc46ca156fbb670f1db6e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cfbc4005abd8c100d59",
+    "url": "/hacer-news-clone/static/js/runtime~main.2cfbc400.js"
+  },
+  {
+    "revision": "32b723b2b358c8612b31",
+    "url": "/hacer-news-clone/static/js/main.32b723b2.chunk.js"
+  },
+  {
+    "revision": "455fcd2de9d1155603d0",
+    "url": "/hacer-news-clone/static/js/1.455fcd2d.chunk.js"
+  },
+  {
+    "revision": "8e40a17211767cde4b2594d9e8edefd4",
+    "url": "/hacer-news-clone/index.html"
+  }
+];
